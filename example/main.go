5010f9c71969dd316bedba64f22ab997f2fd597a
Hui Chen <unmerged@gmail.com>
2015-11-08 22:14:40 +0800

    add getN test

--- example/main.go
+++ example/main.go
@@ -12,26 +12,55 @@ import (
 )
 
 func main() {
-	c := consistent.New()
-	c.Add("127.9.9.3:3322")
-	c.Add("127.9.9.3:3323")
-	c.Add("127.9.9.3:3324")
-	c.Add("127.9.9.3:3325")
-	c.Add("127.9.9.3:3326")
-	c.Add("127.9.9.3:3327")
-	c.Add("127.9.9.3:3328")
-	c.Add("127.9.9.3:3329")
-	servers := make(map[string]int32)
-	for i := 0; i < 100000; i++ {
-		n := rand.Intn(1000000000)
-		key := fmt.Sprintf("%d", n)
-		server, _ := c.Get(key)
-		if v, ok := servers[server]; ok {
-			servers[server] = v + 1
-		} else {
-			servers[server] += 0
+	{
+		c := consistent.New()
+		c.Add("127.9.9.3:3322")
+		c.Add("127.9.9.3:3323")
+		c.Add("127.9.9.3:3324")
+		c.Add("127.9.9.3:3325")
+		c.Add("127.9.9.3:3326")
+		c.Add("127.9.9.3:3327")
+		c.Add("127.9.9.3:3328")
+		c.Add("127.9.9.3:3329")
+		servers := make(map[string]int32)
+		for i := 0; i < 100000; i++ {
+			n := rand.Intn(1000000000)
+			key := fmt.Sprintf("%d", n)
+			server, _ := c.Get(key)
+			if v, ok := servers[server]; ok {
+				servers[server] = v + 1
+			} else {
+				servers[server] += 0
+			}
 		}
+
+		log.Printf("%v", servers)
 	}
 
-	log.Printf("%v", servers)
+	{
+		c := consistent.New()
+		c.Add("127.9.9.3:3322")
+		c.Add("127.9.9.3:3323")
+		c.Add("127.9.9.3:3324")
+		c.Add("127.9.9.3:3325")
+		c.Add("127.9.9.3:3326")
+		c.Add("127.9.9.3:3327")
+		c.Add("127.9.9.3:3328")
+		c.Add("127.9.9.3:3329")
+		servers := make(map[string]int32)
+		for i := 0; i < 100000; i++ {
+			n := rand.Intn(1000000000)
+			key := fmt.Sprintf("%d", n)
+			server, _ := c.GetN(key, 2)
+			for i := 0; i < 2; i++ {
+				if v, ok := servers[server[i]]; ok {
+					servers[server[i]] = v + 1
+				} else {
+					servers[server[i]] += 0
+				}
+			}
+		}
+
+		log.Printf("%v", servers)
+	}
 }
